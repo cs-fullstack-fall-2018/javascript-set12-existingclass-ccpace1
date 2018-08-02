587339e63cee7a8a3b0db28a2a2ea3e8e65fff9b
Christopher Pace <Christopher_pace@ymail.com>
2018-08-02 12:04:21 -0500

    Initial Commit

--- Account.js
+++ Account.js
@@ -21,7 +21,19 @@ class Account{
         return this.accountBalance;
     }
 }
-
+ function ex29(){
+     var newAccount = new Account("Cody", 100);
+     newAccount.deposit(20);
+     document.write(newAccount.balance());
+ }
 function main() {
-
+    //ex30
+var account1 = new Account("Matt's Account", 1000);
+var account2 = new Account("My Account", 0);
+account1.withdrawal(100);
+account2.deposit(100);
+document.write(account1.balance());
+document.write(account2.balance());
 }
+
+main();
--- Person.js
+++ Person.js
@@ -22,7 +22,17 @@ class Person {
 }
 
 function main() {
-
+var cody = new Person("Cody", 23, 250, 74);
+var reco = new Person("Reco", 19, 155, 72);
+var peopleList = [cody,reco];
+cody.printPerson();
+reco.printPerson();
+document.write(peopleList[0]);
+document.write(peopleList[1]);
+console.log("Cody's BMI is" + cody.bodyMassIndex());
+console.log("Reco's BMI is" + reco.bodyMassIndex());
 }
 
 main();
+
+
